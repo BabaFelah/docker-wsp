1ed910a0deb6ff830e0b007143575cc343636315
BabaFelah <messagekerim@gmail.com>
2026-01-15 04:18:38 +0000

    Dockerized data ingestion

--- pipeline/homework/solutions.md
+++ pipeline/homework/solutions.md
@@ -7,7 +7,7 @@ root@3b6d44897b7a:/# pip --version
 pip 25.3 from /usr/local/lib/python3.13/site-packages/pip (python 3.13)
 ```
 
-###2. Understanding Docker networking and docker-compose
+### 2. Understanding Docker networking and docker-compose
 Given the following docker-compose.yaml, what is the hostname and port that pgadmin should use to connect to the postgres database?
 ```
 services:
@@ -40,3 +40,15 @@ volumes:
   vol-pgadmin_data:
     name: vol-pgadmin_data
 ```    
+
+### 3. For the trips in November 2025, how many trips had a trip_distance of less than or equal to 1 mile?
+![alt text](Q3.png)
+
+### 4. Which was the pick up day with the longest trip distance? Only consider trips with trip_distance less than 100 miles. 
+![alt text](Q4.png)
+
+### 5. Which was the pickup zone with the largest total_amount (sum of all trips) on November 18th, 2025?
+![alt text](Q5.png)
+
+### 6. For the passengers picked up in the zone named "East Harlem North" in November 2025, which was the drop off zone that had the largest tip?
+![alt text](Q6.png)
--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,19 +1,35 @@
 import pandas as pd
 import pyarrow.parquet as pq
 from sqlalchemy import create_engine
-
-engine = create_engine('postgresql://postgres:postgres@localhost:5432/ny_taxi')
-
-# Data file
-url = "https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2025-11.parquet"
-df = pd.read_parquet(url)
-df.to_sql(name="green_taxi_data", con=engine, if_exists="replace", chunksize=100000, index=False)
-
-# Lookup table
-url_lkp = "/workspaces/docker-wsp/pipeline/data/taxi_zone_lookup.csv"
-df_lkp = pd.read_csv(url_lkp, delimiter=",", header='infer')
-df_lkp.to_sql(name="taxi_zone", con=engine, if_exists="replace", index=False)
+import click
 
 
+@click.command()
+@click.option('--pg-user', default='postgres', help='PostgreSQL username')
+@click.option('--pg-password', default='postgres', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default='5432', help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database')
+@click.option('--year', default=2025, type=int, help='Year of data')
+@click.option('--month', default=11, type=int, help='Month of data')
 
+
+
+def run(pg_user, pg_password, pg_host, pg_port, pg_db, year, month):
+    engine = create_engine(f'postgresql://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_db}')
+
+    # Lookup table
+    url_lkp = "./data/taxi_zone_lookup.csv"
+    df_lkp = pd.read_csv(url_lkp, delimiter=",", header='infer')
+    df_lkp.to_sql(name="taxi_zone", con=engine, if_exists="replace", index=False)
+
+    # Data file
+    url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_{year}-{month:02d}.parquet"
+    df = pd.read_parquet(url)
+    df.to_sql(name="green_taxi_data", con=engine, if_exists="replace", chunksize=100000, index=False)    
+
+
+
+if __name__ == '__main__':    
+    run()
 
